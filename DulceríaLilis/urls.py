742b23b0c0442278a8baae046b22d4f804aab35f
unknown <benjamin.silva39@inacapmail.cl>
2025-09-04 09:42:37 -0400

    Templates y urls basicas añadidos

--- DulceríaLilis/urls.py
+++ DulceríaLilis/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Dulceria import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('rrss/', views.redes, name="redes"),
+    path('mision/', views.mision, name="mision"),
 ]
+
