e5d7a23aefb88b7124504e2fe36aec1848e15074
unknown <benjamin.silva39@inacapmail.cl>
2025-09-06 09:41:45 -0400

    Subcatalogos y detalles añadidos

--- Dulceria/views.py
+++ Dulceria/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
-
-
-def redes(request):
-    return render(request, 'InfoEmpresa/rrss.html')
-
-def mision(request):
-    return render(request, 'InfoEmpresa/mision.html')
+from .models import productos
 
 def historia_empresa(request):
     return render(request, 'InfoEmpresa/historia_empresa.html')
 
 def catalogo(request):
-    return render(request, 'InfoEmpresa/catalogo.html')
+    categorias = list(productos.keys())
+    return render(request, 'InfoEmpresa/catalogo.html', {'categorias' : categorias})
+
+def subcatalogo(request, categoria):
+    productoCategoria = productos.get(categoria, [])
+    return render(request, 'InfoEmpresa/subcatalogo.html', {
+        'categoria' : categoria,
+        'productos' : productoCategoria
+    })
+
+def detalle(request, categoria, nombreProducto):
+    categoriaProductos = productos.get(categoria, [])
+    producto = next((producto for producto in categoriaProductos if producto["nombre"] == nombreProducto), None)
+    return render(request, 'InfoEmpresa/producto.html', {
+        'producto' : producto,
+        'categoria' : categoria
+    })
--- DulceríaLilis/urls.py
+++ DulceríaLilis/urls.py
@@ -21,9 +21,10 @@ from Dulceria import views
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('rrss/', views.redes, name="redes"),
-    path('mision/', views.mision, name="mision"),
     path('historia-empresa/',views.historia_empresa, name="historia-empresa"),
-    path('catalogo/',views.catalogo, name="catalogo")
+    path('catalogo/',views.catalogo, name="catalogo"),
+    path('catalogo/<str:categoria>/',views.subcatalogo, name="subcatalogo"),
+    path('catalogo/<str:categoria>/<str:nombreProducto>/',views.detalle, name="detalle_producto"),
+    
 ]
 
